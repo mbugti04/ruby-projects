a0876f2db5f7c51e3fd10776a789e05d345b62f0
mbugti04 <mustafammbg@gmail.com>
2024-06-03 20:31:01 -0400

    finished cipher

---
+++ Caesar Cipher/cipher.rb
@@ -0,0 +1,58 @@
+def caesar_cipher(string, shift)
+  ord_arr = string.split('').map do |char|
+    # only convert if letter
+    if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+      char.ord
+    else 
+      char
+    end
+  end
+  # p ord_arr
+
+  shift_arr = ord_arr.map do |int|
+    # p int
+    if int.is_a? Numeric
+      # if upper case
+      if int.between?(65, 90)
+        # can't shift more than the letters of the alphabet
+        shift = shift % 26
+        # wrap around
+        if int + shift > 90
+          int = ((int + shift) % 91) + 65
+          # no need to wrap around
+        else 
+          int = ((int + shift) % 91)
+        end
+        
+      elsif int.between?(97, 122)
+        shift = shift % 26
+        if int + shift > 122
+          int = ((int + shift) % 123) + 97
+        else 
+          int = ((int + shift) % 123)
+        end
+      end
+    else
+      int
+    end
+  end
+
+  # puts "shift_arr"
+  # p shift_arr
+
+  cipher_string = shift_arr.map do |int|
+    if int.is_a? Numeric
+      int.chr
+    else
+      int
+    end
+  end
+
+  cipher_string = cipher_string.join("")
+  # p cipher_string
+end
+
+caesar_cipher("What a string!", 5)
+# caesar_cipher("AaZz", 1)
+# caesar_cipher("GDKKN", 27)
+# caesar_cipher("AZ!!!*(&#*&,.,,.)AKGJALKBJ", 1)
---
+++ Caesar Cipher/spec/cipher_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+require_relative '../cipher'
+
+RSpec.describe 'Cipher Exercises' do
+  
+  describe 'create cipher' do
+
+    it 'returns the correct cipher' do
+      cipher = caesar_cipher("What a string!", 5)
+      result = "Bmfy f xywnsl!"
+    
+      expect(cipher).to eq(result)
+    end
+  end
+end
